6ed8b7cace8efb9104643b3cbb47097e424ad360
AngryBacteria <tctfoxi@gmail.com>
2022-10-27 21:07:13 +0200

    Deleted all old files

--- src/main/java/angryb/LogAPI.java
+++ src/main/java/angryb/LogAPI.java
@@ -1,15 +1,10 @@
 package angryb;
 
-import angryb.old.model.Helper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 @SpringBootApplication
 public class LogAPI {
@@ -17,7 +12,7 @@ public class LogAPI {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
@@ -26,23 +21,6 @@ public class LogAPI {
     }
 
     public static void main(String[] args) {
-
-        Helper helper = new Helper();
-        TimerTask repeatedTask = new TimerTask() {
-            public void run() {
-
-                helper.addGamesToAllPlayers();
-                helper.updateAllPlayers();
-                helper.updateMetadata();
-                System.out.println("SCHEDULED TASK EXECUTED");
-            }
-        };
-        Timer timer = new Timer("Timer");
-
-        long delay = 1000L;
-        long day = 1000L * 60L * 60L * 24L;
-
-        timer.scheduleAtFixedRate(repeatedTask, delay, day);
         SpringApplication.run(LogAPI.class, args);
     }
 }
